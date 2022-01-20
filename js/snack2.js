7fa411c6fd689b951fb2a5d50b19eea944b41a4b
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-20 16:06:42 +0100

    add rand num function for snack2

--- js/snack2.js
+++ js/snack2.js
@@ -5,3 +5,10 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+function getRndNum(min, max) {
+    const rndNum = Math.floor(Math.random() * max - min + 1) + min;
+    return rndNum
+}
+
+console.log(getRndNum(1, 100))
