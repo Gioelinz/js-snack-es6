64e9a928a9e3a699c0e464bb32184f1b66e9682d
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-20 15:18:16 +0100

    snack 1 add first version

--- js/snack1.js
+++ js/snack1.js
@@ -3,3 +3,23 @@ Snack 1
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare ain console la bici con peso minore utilizzando destructuring e template literal */
+
+const bikes = [
+    { nome: "Passeggio", peso: 10 },
+    { nome: "Corsa", peso: 5 },
+    { nome: "Mountain-Bike", peso: 14 },
+]
+
+let arrayNum = [];
+
+//# giro per ogni oggetto nell array
+for (let i = 0; i < bikes.length; i++) {
+    const currentBike = bikes[i];
+    const { peso } = currentBike;
+    arrayNum.push(peso)
+    console.log(arrayNum)
+
+}
+
+//
+console.log(Math.min(...arrayNum))
