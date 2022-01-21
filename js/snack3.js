d03bda9ca3d2705615db645d293598f13e6ed4f6
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-21 16:15:41 +0100

    add snack3 solution

--- js/snack3.js
+++ js/snack3.js
@@ -3,3 +3,17 @@ Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più p
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 Usiamo i nuovi metodi degli array visti oggi in classe.
  */
+
+const arrCars = ['Fiat', 'Lamborghini', 'Ferrari', 'KIA', 'Toyota', 'BMW'];
+
+
+const includesFunction = (arr, a, b) => {
+    const newArray = arr.filter((item, index) => {
+        if (index >= a && index <= b) return true;
+        return false;
+    });
+    return newArray
+};
+
+
+console.log(includesFunction(arrCars, 2, 3));
